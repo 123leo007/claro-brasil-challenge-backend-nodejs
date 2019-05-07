7317370c4b0762ae80eee4d2c234d40a4dceadcd
Leonardo <leonardo.ribeiro@spread.com.br>
2019-05-07 11:25:44 -0300

    gulp nodemon browser-sync configured

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync');
+
+var nodemon = require('gulp-nodemon');
+
+gulp.task('nodemon', function (cb) {
+    var started = false;
+
+    return nodemon({
+          script: 'app.js'
+    }).on('start', function () {
+          if (!started) {
+              cb();
+              started = true;
+          }
+    });
+});
+
+gulp.task('browser-sync', gulp.series( ['nodemon'], function() {
+browserSync.init(null, {
+       proxy: "http://localhost:8000",
+                files: ["public/**/*.*"],
+                port: 3000,
+       });
+}));
+
+gulp.task('default', gulp.series( ['browser-sync'], function () {
+}));
